9b45b6df4d8618199b559b3ba6c8364daa2aeeae
Mar.io <34627453+Maar-io@users.noreply.github.com>
2023-06-23 13:13:25 +0200

    fix year

--- frontend/src/components/TopInfo.tsx
+++ frontend/src/components/TopInfo.tsx
@@ -2,7 +2,7 @@ export const TopInfo = () => {
   return (
     <div className="group">
       <div className="text-info">
-        Pink Portal opens on Tuesday 27th July 2021 at 8am UTC
+        Pink Portal opens on Tuesday 27th July 2023 at 8am UTC
         <br/>
         </div>
     </div>
